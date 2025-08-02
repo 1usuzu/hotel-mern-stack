52824ecff681c14b21b30411204d14e21bd110e6
Dũng Lưu Xuân <dunghoa040705@gmail.com>
2025-08-02 23:52:07 +0700

    thêm docker cho backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,10 +6,18 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    console.log('\n' + '--------------------');
+    console.log(`\x1b[32m\x1b[1m%s\x1b[0m`, `✅ MongoDB Connected!`);
+    console.log(`\x1b[1mHost:\x1b[0m ${conn.connection.host}`);
+    console.log('--------------------' + '\n');
+
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error('\n' + '--------------------');
+    console.error(`\x1b[31m\x1b[1m%s\x1b[0m`, `❌ MongoDB Connection Failed!`);
+    console.error(`\x1b[1mError:\x1b[0m ${error.message}`);
+    console.error('--------------------' + '\n');
+    process.exit(1); // Thoát khỏi tiến trình với mã lỗi
   }
 };
 
